dd1efb91ec76d8a46f9356f2778f23ae874ac2f2
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-04-15 17:39:50 -0500

    Add images in slider add css and js for slider

--- script.js
+++ script.js
@@ -29,32 +29,21 @@ function init(){
   google.maps.event.addListener(marker, 'mouseover', function() {
       infowindow.open(myMap, marker);
     });
-
-
 }
 
 google.maps.event.addDomListener(window, 'load', init);
-          
+   
+
+/*For the slider*/
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'vertical',
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
 
 
