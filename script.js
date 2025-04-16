f4f8e776d47b38b33098c174494ed2a162bb88e6
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-04-15 19:37:38 -0500

    trying to fix slider

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 51.500857, lng: 0.124598},
-        zoom: 15,
-    });
+<script>
+  // Make the slider (no autoplay at first)
+  var swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
-    const marker = new google.maps.Marker({
-        map,
-        position: {lat: 51.5007, lng: 0.1246},
-        title: "Big Ben",
-    });
-}
+  // When Slide 1 is clicked, start autoplay
+  document.getElementById('slide-1').onclick = function () {
+    swiper.params.autoplay = {
+      delay: 5000,
+      disableOnInteraction: false,
+    };
+    swiper.autoplay.start();
+  };
+</script>
 
 /*For the slider*/
 const swiper = new Swiper('.swiper', {
+  // Optional parameters
   direction: 'vertical',
   loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
+
+
 
 
