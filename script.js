2ada5bbd5c94211942fc5387c3d35b5229939d74
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-04-15 19:17:13 -0500

    change coordinates

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 41.836449, lng: -87.626691},
+        center: {lat: 51.500857, lng: 0.124598},
         zoom: 15,
     });
 
     const marker = new google.maps.Marker({
         map,
-        position: {lat: 41.838615380326466, lng: -87.63213314132148},
-        title: "Ferro's",
+        position: {lat: 51.5007, lng: 0.1246},
+        title: "Big Ben",
     });
 }
 
