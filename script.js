754169f8dd9331baeb65d322d7acb27a9a2d32e9
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-04-29 02:28:26 -0500

    fix marker to appear

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 51.500857, lng: 0.124598},
-    zoom: 15,
-  });
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 51.500857, lng: 0.124598},
+        zoom: 15,
+    });
 
   const marker = new google.maps.Marker({
     map,
     position: {lat: 51.5007, lng: 0.1246},
     title: "Big Ben",
-    icon: "media/bigBenMarker.png",
   });
 
   const infoWindow = new google.maps.InfoWindow({
@@ -21,15 +20,15 @@ function initMap() {
 
 }
 
-var swiper = new Swiper('.swiper', {
-  loop: true,
-  autoplay: {
-  delay: 5000,
-},
+  var swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+    delay: 5000,
+  },
  
   navigation: {
-    nextEl: '.swiper-button-next', 
-    prevEl: '.swiper-button-prev',  
+           nextEl: '.swiper-button-next', 
+           prevEl: '.swiper-button-prev',  
   },
 });
 
